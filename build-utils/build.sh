79aeef7141b8f5bf87b7a0bd89408ba617cbd190
Douglas Bentley <dee.bent.lee@gmail.com>
2016-04-09 09:04:21 +0200

    Added a build script for future ease of use

---
+++ build-utils/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# use browserify to bundle javascript dependencies together nicely
+# thanks obama
+watchify ../scripts/animation.js \
+		 ../scripts/backdrop.js \
+		 ../scripts/config.js \
+		 ../scripts/game.js \
+		 ../scripts/gamesquare.js \
+		 ../scripts/gamestate.js \
+		 ../scripts/lib/seedrandom.min.js \
+		 ../scripts/lib/three.min.js \
+		 ../scripts/materials.js \
+		 ../scripts/score.js \
+		 ../scripts/square.js \
+		 ../scripts/treenode.js \
+		 ../scripts/utilities.js \
+		 -o ../scripts/bundle.js
